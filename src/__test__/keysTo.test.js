106efcfd1a15f236d148115170e73549fe0f546e
Arif Setyawan <me@arifsetyawan.com>
2019-10-05 12:53:24 +0800

    basic implementation

--- src/__test__/keysTo.test.js
+++ src/__test__/keysTo.test.js
@@ -1,3 +1,117 @@
-describe('testing ', function () {
-  
+import { setKey }  from '../keysTo';
+
+describe('keysTo', function () {
+  it('camel to snake case on complex input', function () {
+    let targetObj = {
+      name: "Arif",
+      livingAddress: "Pondok Karya Agung",
+      itemOnHand: {
+        weapon: "sword",
+        armor: "chainmail"
+      },
+      itemOnBag: [
+        {
+          itemName: "potion",
+          itemQty: 1
+        },
+        {
+          itemName: "antidote",
+          itemQty: 4
+        }
+      ]
+    }
+    let newObj = setKey(targetObj).toSnakeCase();
+    console.log(newObj);
+  });
+
+  it('camel to snake case on input array', function () {
+    let targetObj = [
+        {
+          itemName: "potion",
+          itemQty: 1
+        },
+        {
+          itemName: "antidote",
+          itemQty: 4
+        }
+      ];
+    let newObj = setKey(targetObj).toSnakeCase();
+    console.log(newObj);
+  });
+
+  it('snake to camel case on complex input', function () {
+    let targetObj = {
+      name: "Arif",
+      living_address: "Pondok Karya Agung",
+      item_on_hand: {
+        weapon: "sword",
+        armor: "chainmail"
+      },
+      item_on_bag: [
+        {
+          item_name: "potion",
+          item_qty: 1
+        },
+        {
+          item_name: "antidote",
+          item_qty: 4
+        }
+      ]
+    }
+    let newObj = setKey(targetObj).toCamelCase();
+    console.log(newObj);
+  });
+
+  it('snake to camel case on input array', function () {
+    let targetObj = [
+      {
+        item_name: "potion",
+        item_qty: 1
+      },
+      {
+        item_name: "antidote",
+        item_qty: 4
+      }
+    ];
+    let newObj = setKey(targetObj).toCamelCase();
+    console.log(newObj);
+  });
+
+  it('mix to camel case on complex input', function () {
+    let targetObj = {
+      name: "Arif",
+      living_address: "Pondok Karya Agung",
+      itemOnHand: {
+        weapon: "sword",
+        armor: "chainmail"
+      },
+      item_on_bag: [
+        {
+          itemName: "potion",
+          itemQty: 1
+        },
+        {
+          item_name: "antidote",
+          item_qty: 4
+        }
+      ]
+    }
+    let newObj = setKey(targetObj).toCamelCase();
+    console.log(newObj);
+  });
+
+  it('empty Obj', function () {
+    let targetObj = {};
+    let newObj = setKey(targetObj).toCamelCase();
+  });
+
+  it('empty Array', function () {
+    let targetObj = [];
+    let newObj = setKey(targetObj).toCamelCase();
+  });
+
+  it('no params', function () {
+    let newObj = setKey().toCamelCase();
+  });
+
 });
--- src/keysTo.js
+++ src/keysTo.js
@@ -1,14 +1,46 @@
-/**
- *
- * @param inputObj
- * @param transForm
- * @param options
- */
-export function keysTo(inputObj, transForm, options) {
+import strCase from 'case';
 
-  console.log(Object.keys(inputObj));
-  return true;
-
+export function setKey(inputObj) {
+  let keyToObj = new KeyTo(inputObj);
+  return keyToObj;
 };
 
+class KeyTo {
 
+  get objectTo() {
+    return this._objectTo;
+  }
+
+  set objectTo(value) {
+    this._objectTo = value;
+  }
+
+  _objectTo;
+
+  constructor(objectTo) {
+    this.objectTo = objectTo;
+  }
+
+  toCamelCase() {
+    return this::transform(this.objectTo, strCase.camel)
+  }
+
+  toSnakeCase() {
+    return this::transform(this.objectTo, strCase.snake);
+  }
+
+}
+
+function transform(targetObj, method) {
+  let newObj = {}
+  if (!targetObj) return false;
+  if (targetObj && targetObj.length === 0) return false;
+  for (let key in targetObj) {
+    if (typeof targetObj[key] === 'object') {
+      newObj[method(key)] = this::transform(targetObj[key], method);
+    } else {
+      newObj[method(key)] = targetObj[key];
+    }
+  }
+  return newObj;
+}
