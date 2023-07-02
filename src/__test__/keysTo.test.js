3941b5f3b084b388f560391b2b3533f65a81677d
arifsetyawan <me@arifsetyawan.com>
2023-07-02 20:57:07 +0700

    fix: functionality to handle array

--- src/__test__/keysTo.test.js
+++ src/__test__/keysTo.test.js
@@ -1,117 +1,153 @@
 const keysTo = require("../keysTo");
 
-describe('keysTo', function () {
-  it('camel to snake case on complex input', function () {
+describe("keysTo", function () {
+  it("camel to snake case on complex input", function () {
     let targetObj = {
       name: "Arif",
       livingAddress: "Pondok Karya Agung",
       itemOnHand: {
         weapon: "sword",
-        armor: "chainmail"
+        armor: "chainmail",
       },
       itemOnBag: [
         {
           itemName: "potion",
-          itemQty: 1
+          itemQty: 1,
         },
         {
           itemName: "antidote",
-          itemQty: 4
-        }
-      ]
-    }
+          itemQty: 4,
+        },
+      ],
+    };
     let newObj = keysTo.setKey(targetObj).toSnakeCase();
     console.log(newObj);
   });
 
-  it('camel to snake case on input array', function () {
+  it("camel to snake case on input array", function () {
     let targetObj = [
-        {
-          itemName: "potion",
-          itemQty: 1
-        },
-        {
-          itemName: "antidote",
-          itemQty: 4
-        }
-      ];
+      {
+        itemName: "potion",
+        itemQty: 1,
+        sub: [
+          {
+            currentScore: 50,
+            metaData: [
+              { id: "1", levelDungeon: "medium"},
+              { id: "2", levelDungeon: "medium"}
+            ],
+            tags: ["beta", "alpha"]
+          },
+          {
+            currentScore: 25,
+            metaData: [
+              { id: "1", levelDungeon: "hard"},
+              { id: "2", levelDungeon: "medium"}
+            ],
+            tags: ["chime", "yolo"]
+          },
+        ]
+      },
+      {
+        itemName: "antidote",
+        itemQty: 4,
+        sub: [
+          {
+            currentScore: 50,
+            metaData: [
+              { id: "1", levelDungeon: "low"},
+              { id: "2", levelDungeon: "medium"}
+            ],
+            tags: ["beta", "omega"]
+          },
+          {
+            currentScore: 25,
+            metaData: [
+              { id: "1", levelDungeon: "low"},
+              { id: "2", levelDungeon: "hard"}
+            ],
+            tags: ["kiku", "yolo"]
+          },
+        ]
+      },
+    ];
+
     let newObj = keysTo.setKey(targetObj).toSnakeCase();
-    console.log(newObj);
+    console.log(JSON.stringify(newObj));
   });
 
-  it('snake to camel case on complex input', function () {
+  it("snake to camel case on complex input", function () {
     let targetObj = {
       name: "Arif",
       living_address: "Pondok Karya Agung",
       item_on_hand: {
         weapon: "sword",
-        armor: "chainmail"
+        armor: "chainmail",
       },
       item_on_bag: [
         {
           item_name: "potion",
-          item_qty: 1
+          item_qty: 1,
         },
         {
           item_name: "antidote",
-          item_qty: 4
-        }
-      ]
-    }
+          item_qty: 4,
+        },
+      ],
+    };
     let newObj = keysTo.setKey(targetObj).toCamelCase();
     console.log(newObj);
   });
 
-  it('snake to camel case on input array', function () {
+  it("snake to camel case on input array", function () {
     let targetObj = [
       {
         item_name: "potion",
-        item_qty: 1
+        item_qty: 1,
       },
       {
         item_name: "antidote",
-        item_qty: 4
-      }
+        item_qty: 4,
+      },
     ];
     let newObj = keysTo.setKey(targetObj).toCamelCase();
     console.log(newObj);
   });
 
-  it('mix to camel case on complex input', function () {
+  it("mix to camel case on complex input", function () {
     let targetObj = {
       name: "Arif",
       living_address: "Pondok Karya Agung",
       itemOnHand: {
         weapon: "sword",
-        armor: "chainmail"
+        armor: "chainmail",
       },
       item_on_bag: [
         {
           itemName: "potion",
-          itemQty: 1
+          itemQty: 1,
         },
         {
           item_name: "antidote",
-          item_qty: 4
-        }
-      ]
-    }
+          item_qty: 4,
+        },
+      ],
+    };
     let newObj = keysTo.setKey(targetObj).toCamelCase();
     console.log(newObj);
   });
 
-  it('empty Obj', function () {
+  it("empty Obj", function () {
     let targetObj = {};
     let newObj = keysTo.setKey(targetObj).toCamelCase();
   });
 
-  it('empty Array', function () {
+  it("empty Array", function () {
     let targetObj = [];
     let newObj = keysTo.setKey(targetObj).toCamelCase();
   });
 
-  it('no params', function () {
+  it("no params", function () {
     let newObj = keysTo.setKey().toCamelCase();
   });
-
 });
--- src/keysTo.js
+++ src/keysTo.js
@@ -1,27 +1,41 @@
-const strCase = require('case');
+const strCase = require("case");
 
 module.exports.setKey = (inputObj) => {
   let transformOption = {
     toCamelCase() {
-      return transform(inputObj, strCase.camel)
+      return transform(inputObj, strCase.camel);
     },
     toSnakeCase() {
-      return transform(inputObj, strCase.snake)
-    }
+      return transform(inputObj, strCase.snake);
+    },
   };
   return transformOption;
 };
 
 const transform = (targetObj, method) => {
-  let newObj = {}
+  let newObj = {};
   if (!targetObj) return false;
+
   if (targetObj && targetObj.length === 0) return false;
-  for (let key in targetObj) {
-    if (typeof targetObj[key] === 'object') {
-      newObj[method(key)] = transform(targetObj[key], method);
-    } else {
-      newObj[method(key)] = targetObj[key];
+
+  if (targetObj && typeof targetObj === "object" && !Array.isArray(targetObj)) {
+    for (let key in targetObj) {
+      if (typeof targetObj[key] === "object") {
+        newObj[method(key)] = transform(targetObj[key], method);
+      } else {
+        newObj[method(key)] = targetObj[key];
+      }
     }
   }
+
+  if (targetObj && Array.isArray(targetObj)) {
+    newObj = targetObj.map((item) => {
+      if (typeof item === "object") {
+        return transform(item, method);
+      }
+      return item;
+    });
+  }
+
   return newObj;
-}
+};
